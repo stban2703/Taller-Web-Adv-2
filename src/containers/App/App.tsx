1471573c322754362773a85ca759fc1d2b1bf093
stban2703 <s.t.ban1401@gmail.com>
2021-06-08 01:37:28 -0500

    First commit

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+
+export const App = () => {
+    return (
+        <main>
+            <HashRouter>
+                
+            </HashRouter>
+        </main>
+    )
+}
